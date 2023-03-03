67bb731d5fa320a0fab315fa50c13dcc11a1cb51
Pablo Aller <pablusprogramador@gmail.com>
2023-03-03 19:25:30 +0100

    .

---
+++ react-fetch-pro/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useFetch } from "./useFetch";
+
+export function App() {
+  const { data, loading, error, handleCancelRequest } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return (
+    <div className="App">
+      <h1>Fetch like a pro</h1>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
+      <div className="card">
+        <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
